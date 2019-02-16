1f6bda0362eab91c51f5352a771dd1873a14cf59
Alexander Kneipp <alexander.h.kneipp@gmail.com>
2019-02-16 13:56:23 -0500

    Stubs out methods

---
+++ src/ImgProc.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+
+
+def get_colors(img, blob_pts):
+    pass
+
+
+def get_intensity(img, blob_pts):
+    pass
+
+
+def get_saturation(img, blob_pts):
+    pass
+
+
+def get_size(img, blob_pts):
+    pass
+
+
+def get_noisiness(img, blob_pts):
+    pass
+
+
+
