dee14fe6802de9a78e32b97160ab8f7308fa3713
Alexander Kneipp <alexander.h.kneipp@gmail.com>
2019-02-16 16:52:53 -0500

    Generates masks for breaking up the input image

--- src/ImgProc.py
+++ src/ImgProc.py
@@ -3,6 +3,7 @@ import numpy as np
 
 
 def get_colors(img, blob_pts):
+    
     pass
 
 
@@ -21,5 +22,33 @@ def get_size(img, blob_pts):
 def get_noisiness(img, blob_pts):
     pass
 
+
+def get_img_maps(img, num_blocks):
+    greyscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_height, img_width = np.shape(greyscale)
+    img_height = int(img_height)
+    img_width = int(img_width)
+    depth = num_blocks ** 2
+    retval = np.zeros((img_height, img_width, depth), dtype="uint8")
+    height_step = img_height // num_blocks
+    width_step = img_width // num_blocks
+    for i in range(num_blocks):
+        for j in range(num_blocks):
+            this_depth = (i * (num_blocks) + j)
+            retval[i * height_step:((i + 1) * height_step), j * width_step: ((j+1) * width_step), this_depth] = \
+            np.ones((height_step, width_step), dtype="uint8")
+    return retval
+
+
+
+    return retval
+
+img = cv2.imread("../index.jpeg")
+masks = get_img_maps(img, 16)
+x, y, z = np.shape(masks)
+for i in range(z):
+    cv2.imshow("img", np.multiply(img, np.expand_dims(masks[::, ::, i], axis=2)))
+    print (i)
+    cv2.waitKey(0)
 
 
