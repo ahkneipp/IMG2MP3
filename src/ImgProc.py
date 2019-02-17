61a58c158516eca66084f244902b8c925ddd1cfe
Alexander Kneipp <alexander.h.kneipp@gmail.com>
2019-02-17 01:00:20 -0500

    adds get_data function

--- src/ImgProc.py
+++ src/ImgProc.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
+import collections
 
-# TODO pass highest bucket back out
 def get_colors(img, mask):
     hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     # hsv_img = np.multiply(hsv_img, np.expand_dims(masks[::, ::, i], axis=2))
@@ -9,18 +9,17 @@ def get_colors(img, mask):
     hist = cv2.calcHist([hsv_img], channels=[0], mask=mask, histSize=[12], ranges=[0, 179])
     return np.argmax(hist)
 
-# TODO Get the max value here
 def get_intensity(img, mask):
     hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     # hsv_img = np.multiply(hsv_img, np.expand_dims(masks[::, ::, i], axis=2))
-    hist = cv2.calcHist([hsv_img], channels=[2], mask=mask, histSize=[20], ranges=[0, 255])
+    hist = cv2.calcHist([hsv_img], channels=[2], mask=mask, histSize=[2], ranges=[0, 255])
     return np.argmax(hist)
 
 
 def get_saturation(img, mask):
     hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     # hsv_img = np.multiply(hsv_img, np.expand_dims(masks[::, ::, i], axis=2))
-    hist = cv2.calcHist([hsv_img], channels=[1], mask=mask, histSize=[2], ranges=[0, 255])
+    hist = cv2.calcHist([hsv_img], channels=[1], mask=mask, histSize=[4], ranges=[0, 255])
     return np.argmax(hist)
 
 
@@ -33,7 +32,7 @@ def get_noisiness(img, mask):
     indices = np.meshgrid(np.arange(min(i), max(i) + 1),
                           np.arange(min(j), max(j) + 1),
                           indexing='ij')
-    sub_img = img[indices]
+    sub_img = img[tuple(indices)]
 
     # Pretty much straight copied from https://www.pyimagesearch.com/2016/07/25/convolutions-with-opencv-and-python/
     gray_img = cv2.cvtColor(sub_img, cv2.COLOR_BGR2GRAY)
@@ -50,7 +49,7 @@ def get_noisiness(img, mask):
             k = (roi * kernel).sum()
 
             output[y-pad, x-pad] = abs(k)
-    return int(np.average(output) * 6/43.0)
+    return int(np.average(output) * 24/43.0)
 
 
 def get_img_maps(img, num_blocks):
@@ -91,24 +90,35 @@ def get_masks(img):
         masks[:,:,i] = tmp
     return masks
 
-img = cv2.imread("../test1.jpg")
+
+ImgData = collections.namedtuple('ImgData', 'size, color, intensity, saturation, noise')
+
+def get_data(img, masks):
+    print("Processing Countours", end="", flush=True)
+    data = []
+    height, width, num_masks = np.shape(masks)
+    for i in range(num_masks):
+        print(".", end="", flush=True)
+        mask = masks[:, :, i]
+        data.append(ImgData(size=get_size(mask),
+                            color=get_colors(img, mask),
+                            intensity=get_intensity(img, mask),
+                            saturation=get_saturation(img, mask),
+                            noise=get_noisiness(img,mask)))
+    return data
+
+# img = cv2.imread("../index.jpeg")
+img = np.ones((500,500,3),dtype="uint8")
 #img = np.random.randint(0,255,size=(500, 500, 3), dtype="uint8")
 #img[:, :, 2] = np.random.randint(255, size=(500,500), dtype="uint8")
+print("Finding Contours...")
 masks = get_masks(img)
 x, y, z = np.shape(masks)
+all_data = get_data(img, masks)
 # print (get_noisiness(np.random.randint(255, size=(1000, 1000, 3), dtype="uint8"), None))
 # print (get_noisiness(np.ones((1000, 1000, 3), dtype="uint8"), None))
 for i in range(z):
     cv2.imshow("img", np.multiply(img, np.expand_dims(masks[::, ::, i], axis=2)))
-    print("Image %d" % (i + 1))
-    tmp_hist = get_colors(img, masks[::, ::, i])
-    print("\tColor Range: %d" % tmp_hist)
-    tmp_intensity = get_intensity(img, masks[::,::,i])
-    print("\tIntensity: %d" % tmp_intensity)
-    tmp_saturation = get_saturation(img, masks[::,::,i])
-    print("\tSaturation: %d" % tmp_saturation)
-    tmp_size = get_size(masks[::,::,i])
-    print("\tSize: %d" % tmp_size)
-    print("\tNoisiness: %d" % (get_noisiness(img, masks[::, ::, i])))
+    print("Data %d: " % i + str(all_data[i]))
     if cv2.waitKey(1) == 27:
         break
