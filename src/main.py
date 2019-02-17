ff7a510ae9110b9cb61fa2605f514dda51af26e6
Alexander Kneipp <alexander.h.kneipp@gmail.com>
2019-02-17 03:37:59 -0500

    Adds testing main code

--- src/main.py
+++ src/main.py
@@ -1,1 +1,23 @@
-print('Hello World')
+import ImgProc
+import synthFuncs
+import cv2
+import numpy as np
+
+cameraCapture = cv2.VideoCapture(0)
+while cameraCapture.isOpened():
+    success, img = cameraCapture.read()
+    cv2.imshow("Test", img)
+    keyPress = cv2.waitKey(0)
+    success = False
+    start = True
+    if keyPress == 27:
+        break
+
+    if success:
+        cv2.imshow("Test", img)
+        start = False
+    elif not start:
+        break
+
+cv2.destroyAllWindows()
+cameraCapture.release()
