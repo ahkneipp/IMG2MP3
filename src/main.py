32ff24d088726b7be2f5ebbab7a86d64c6db8a0a
Alexander Kneipp <alexander.h.kneipp@gmail.com>
2019-02-17 07:16:51 -0500

    Generalizes number of contours to keep

--- src/main.py
+++ src/main.py
@@ -4,11 +4,13 @@ import cv2
 import time
 import numpy as np
 from picamera import PiCamera
+import wiringpi
 
 camera = PiCamera()
 camera.resolution=(320,240)
 time.sleep(0.1)
 
+num_effective_contours = 10
 
 while True:
     keyPress = cv2.waitKey(1)
@@ -18,20 +20,20 @@ while True:
     cv2.imshow("Test", rawCapture)
     if keyPress == 32:
         masks =ImgProc.get_masks(rawCapture, 1)
-        top_ten_indices = [(-1,-1)] * 10
+        top_indices = [(-1,-1)] * num_effective_contours
         x, y, num_masks = np.shape(masks) 
         for i in range(num_masks - 1):
-            current = 9
-            print("%d, %d, %d, %d" % (current, len(top_ten_indices), i, num_masks))
-            while(current >= 0 and ImgProc.get_size(masks[:,:,i]) > top_ten_indices[current][0]):
-                tmp = top_ten_indices[current]
-                top_ten_indices[current] = (i, ImgProc.get_size(masks[:,:,i]))
-                top_ten_indices.insert(current + 1, tmp)
-                top_ten_indices = top_ten_indices[0:10]
+            current = num_effective_contours-1
+            print("%d, %d, %d, %d" % (current, len(top_indices), i, num_masks))
+            while(current >= 0 and ImgProc.get_size(masks[:,:,i]) > top_indices[current][0]):
+                tmp = top_indices[current]
+                top_indices[current] = (i, ImgProc.get_size(masks[:,:,i]))
+                top_indices.insert(current + 1, tmp)
+                top_indices = top_indices[0:num_effective_contours]
                 current = current - 1
-        newMasks = np.zeros((x,y,10), dtype='uint8')
-        for i in range( len(top_ten_indices) ):
-            if i < num_masks and top_ten_indices[i][0] != -1:
+        newMasks = np.zeros((x,y,num_effective_contours), dtype='uint8')
+        for i in range( len(top_indices) ):
+            if i < num_masks and top_indices[i][0] != -1:
                 newMasks[:,:,i] = masks[:,:,i]
             else:
                 newMasks = newMasks[:,:,0:i-1]
@@ -39,7 +41,7 @@ while True:
         for d in data:
             synthFuncs.play_progression(synthFuncs.get_progression(d[1],
                 d[3], d[2], d[4]))
-            time.sleep(.3)
+            time.sleep(.2)
     if keyPress == 27:
         break
 
